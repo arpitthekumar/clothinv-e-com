f3e0af5c016d14300bdd22b381bb435099db01d1
arpitthekumar <sg79720@gmail.com>
2026-01-28 18:48:41 +0530

    clean up

--- drizzle/migrations/0004_add_customer_details.sql
+++
@@ -1,8 +0,0 @@
--- Add customer details columns to sales table
-ALTER TABLE sales
-  ADD COLUMN customer_name TEXT,
-  ADD COLUMN customer_phone TEXT;
-
--- Add indexes for better query performance
-CREATE INDEX idx_sales_customer_phone ON sales(customer_phone);
-CREATE INDEX idx_sales_created_at ON sales(created_at);
